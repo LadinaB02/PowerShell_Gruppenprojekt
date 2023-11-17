68d22ba1a8fdb694f2404b60d087280bb2afe67c
juliaobe <148975597+juliaobe@users.noreply.github.com>
2023-11-17 15:30:37 +0100

    Update Test.ps1
    eifach alles inegschmisse

--- Test.ps1
+++ Test.ps1
@@ -2,3 +2,23 @@
 
 Test-Path putty-64bit-0.79-installer\*.msi
 Test-Path putty_localhost\*.reg
+
+
+
+Stop-Process -Name "Putty"
+
+msiexec /i .\putty-64bit-0.79-installer.msi /qn
+
+Get-Process | Where-Object {$_.ProcessName -like "*Putty*"}
+
+$registryPath = "tty-64bit-0.79-installer.msi"
+
+ if (Test-Path $registryPath) {
+
+     Write-Host "Der Registry-Schlüssel für die Session 'Localhost' existiert."
+
+ } else {
+
+     Write-Host "Der Registry-Schlüssel für die Session 'Localhost' existiert nicht."
+
+ }
